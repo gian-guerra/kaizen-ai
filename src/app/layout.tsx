2fb7a2900f48de8f8ef98d212b33b663137bf34e
gian-guerra <giancguerra0@gmail.com>
2025-02-22 15:11:27 -0500

    Add authHandler component to remove auth token from url (#6)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/app/globals.css"
+import AuthHandler from "@/components/auth-handler"
 import { Inter } from "next/font/google"
 import type React from "react" // Import React
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthHandler />
+        {children}
+      </body>
     </html>
   )
 }
